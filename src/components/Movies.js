c4c64c1f6d03ee2957c99379f4857019c2d0990a
Jose Aguilar <jaguilarny@gmail.com>
2022-07-20 23:34:19 -0400

    Lab complete

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,26 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div>
+      <h1>Movies Page</h1>
+      {movies.map((movie) => (
+        <div>
+          <h3>Title: {movie.title}</h3>
+          <p>Time: {movie.time}</p>
+          <p>Genres:</p>
+          <ul>
+            {movie.genres.map((genre) => (
+              <li>
+                {genre}
+              </li>
+            ))}
+          </ul>
+        </div>
+      ))}
+    </div>
+    
+  )
 }
 
 export default Movies;
